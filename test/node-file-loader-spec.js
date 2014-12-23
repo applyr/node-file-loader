f48aafd545719679a635b2e6fabf2a2bf5ff0216
Jack <jack@applyr.com>
2014-12-23 19:54:56 +0000

    Initial commit

---
+++ test/node-file-loader-spec.js
@@ -0,0 +1,94 @@
+/* jshint -W030 */
+
+require('./helpers');
+
+var Loader = require('../lib');
+
+describe('Loader', function () {
+
+  before(function () {
+    this.loader = new Loader();
+  });
+
+  describe('.load', function () {
+    it('loads the file, if the file exists', function (done) {
+      this.loader.load(__dirname+'/fixtures/.demo')
+      .then(function (file) {
+        var expected = "line1\nline2\nline3\n";
+        expect(file).to.deep.equal(expected);
+        done();
+      });
+    });
+
+    it('returns an error, if the file does not exist', function (done) {
+      this.loader.load(__dirname+'/fixtures/does-not-exist')
+      .fail(function (err) {
+        expect(err).to.be.ok;
+        done();
+      });
+    });
+  });
+
+  describe('.yaml', function () {
+    it('loads the file, if the file exists', function (done) {
+      this.loader.yaml(__dirname+'/fixtures/demo.yml')
+      .then(function (file) {
+        var expected = {
+          users: ["jpstevens", "applyr"],
+          languages: ["javascript", "coffeescript"],
+          projects: { javascript: 12, coffeescript: 7 }
+        };
+        expect(file).to.deep.equal(expected);
+        done();
+      });
+    });
+
+    it('returns an error, if the file does not exist', function (done) {
+      this.loader.yaml(__dirname+'/fixtures/does-not-exist')
+      .fail(function (err) {
+        expect(err).to.be.ok;
+        done();
+      });
+    });
+
+    it('returns an error, if the file cannot be parsed', function (done) {
+      this.loader.yaml(__dirname+'/fixtures/bad-demo.yml')
+      .fail(function (err) {
+        expect(err).to.be.ok;
+        done();
+      });
+    });
+  });
+
+  describe('.json', function () {
+    it('loads the file, if the file exists', function (done) {
+      this.loader.json(__dirname+'/fixtures/demo.json')
+      .then(function (file) {
+        var expected = {
+          users: ["jpstevens", "applyr"],
+          languages: ["javascript", "coffeescript"],
+          projects: { javascript: 12, coffeescript: 7 }
+        };
+        expect(file).to.deep.equal(expected);
+        done();
+      });
+    });
+
+    it('returns an error, if the file does not exist', function (done) {
+      this.loader.json(__dirname+'/fixtures/does-not-exist')
+      .fail(function (err) {
+        expect(err).to.be.ok;
+        done();
+      });
+    });
+
+    it('returns an error, if the file cannot be parsed', function (done) {
+      this.loader.json(__dirname+'/fixtures/bad-demo.yml')
+      .fail(function (err) {
+        expect(err).to.be.ok;
+        done();
+      });
+    });
+  });
+
+});
